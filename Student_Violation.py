c9fcb29e709ab1aaf63599f5009f5c903018220c
harikicks <jxtian1874@gmail.com>
2025-01-13 00:09:21 +0800

    UPDATED

--- Student_Violation.py
+++ Student_Violation.py
@@ -115,6 +115,33 @@ studentname=[{"name":"Africano, Luigi V","section":"BET-CPET-1B","ID":"TUPM-23-2
 
 
 table.insert(parent = '', index = 0, values = (studentname[0]['name'],studentname[0]['section'],studentname[0]['ID'],studentname[0]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[1]['name'],studentname[1]['section'],studentname[1]['ID'],studentname[1]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[2]['name'],studentname[2]['section'],studentname[2]['ID'],studentname[2]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[3]['name'],studentname[3]['section'],studentname[3]['ID'],studentname[3]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[4]['name'],studentname[4]['section'],studentname[4]['ID'],studentname[4]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[5]['name'],studentname[5]['section'],studentname[5]['ID'],studentname[5]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[6]['name'],studentname[6]['section'],studentname[6]['ID'],studentname[6]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[7]['name'],studentname[7]['section'],studentname[7]['ID'],studentname[7]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[8]['name'],studentname[8]['section'],studentname[8]['ID'],studentname[8]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[9]['name'],studentname[9]['section'],studentname[9]['ID'],studentname[9]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[10]['name'],studentname[10]['section'],studentname[10]['ID'],studentname[10]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[11]['name'],studentname[11]['section'],studentname[11]['ID'],studentname[11]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[12]['name'],studentname[12]['section'],studentname[12]['ID'],studentname[12]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[13]['name'],studentname[13]['section'],studentname[13]['ID'],studentname[13]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[14]['name'],studentname[14]['section'],studentname[14]['ID'],studentname[14]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[15]['name'],studentname[15]['section'],studentname[15]['ID'],studentname[15]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[16]['name'],studentname[16]['section'],studentname[16]['ID'],studentname[16]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[17]['name'],studentname[17]['section'],studentname[17]['ID'],studentname[17]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[18]['name'],studentname[18]['section'],studentname[18]['ID'],studentname[18]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[19]['name'],studentname[19]['section'],studentname[19]['ID'],studentname[19]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[20]['name'],studentname[20]['section'],studentname[20]['ID'],studentname[20]['vi']))
+table.insert(parent = '', index = 0, values = (studentname[21]['name'],studentname[21]['section'],studentname[21]['ID'],studentname[21]['vi']))
+
+
+
+
+
+
 table.insert(parent = '', index = 0, values = ('Armendi, Francis M','BET-CPET-1B','TUPM-23-3344','N/A'))
 table.insert(parent = '', index = 0, values = ('Cuerda, Bea Bianca B. ','BET-CPET-1B','TUPM-23-3453','N/A'))
 table.insert(parent = '', index = 0, values = ('De Guzman, Jose Miguel T.','BET-CPET-1B','TUPM-23-3454','N/A'))
